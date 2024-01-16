d7fa60e9d3ad4d4d723b419b8c8695b31205c8d4
Gustavo Thuram <thuramgustavo98@gmail.com>
2024-01-16 12:24:11 -0300

    aa

---
+++ novo.py
@@ -0,0 +1,11 @@
+function faz(n) {
+    var s = n.Tostring();
+    var variavel = 0;
+    for var char of s) {
+        var d = parseInt(char);
+        variavel += d;
+    }
+    return variavel;
+}
+var resultado = faz(123123)
+console.log("Resultado",resultado)
